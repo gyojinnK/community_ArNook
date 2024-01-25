0cac41f073aac36d0b01b532930db6db5e57a4b6
gyojinnK <ksmug1998@gmail.com>
2024-01-25 23:51:54 +0900

    :memo: Update structure
    public 페이지와 private 페이지 라우터 구분

--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -1,34 +1,26 @@
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Navigate,
-    useNavigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/Login/LoginPage";
 import MainPage from "../pages/Main/MainPage";
-import { useEffect, useState } from "react";
+import UsersViewPage from "@/pages/UsersView/UsersViewPage";
+import DetailPage from "@/pages/Detail/DetailPage";
+import PostingPage from "@/pages/Post/PostingPage";
+import { auth } from "@/firebase";
+import PrivateLayout from "./PrivateLayout";
+import PublicLayout from "./PublicLayout";
 
 const Router = () => {
-    const [isLogin, setIsLogin] = useState<boolean>(false);
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     if (!isLoggedIn()) {
-    //         navigate("/");
-    //     }
-    // }, [navigate]);
-
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route
-                    path="/main"
-                    element={
-                        isLogin ? <MainPage /> : <Navigate to="/" replace />
-                    }
-                ></Route>
+                <Route element={<PublicLayout />}>
+                    <Route path="/" element={<LoginPage />} />
+                </Route>
+                <Route element={<PrivateLayout />}>
+                    <Route path="/main" element={<MainPage />}></Route>
+                    <Route path="/usersView" element={<UsersViewPage />} />
+                    <Route path="/detail" element={<DetailPage />} />
+                    <Route path="/posting" element={<PostingPage />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
