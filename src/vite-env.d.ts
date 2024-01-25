242ed3097ef6718d737f3aa72e325119c696750a
gyojinnK <ksmug1998@gmail.com>
2024-01-25 23:59:20 +0900

    :label: Add types
    AuthContextType 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface AuthContextType {
+    state: { isLogin: boolean; email: string };
+    actions: {
+        setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+        setEmail: React.Dispatch<React.SetStateAction<string>>;
+    };
+}
