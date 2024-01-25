a01ced4e7ef32771aba22137df4c65a972d97bd9
gyojinnK <ksmug1998@gmail.com>
2024-01-25 23:58:21 +0900

    :hammer: Update code
    로그인 폼 컴포넌트 추가

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,23 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
+import SignUpFrom from "./SignUpForm";
 
-const LoginPage = () => {
-    return <LoginForm />;
+const LoginPage: React.FC = () => {
+    const navigate = useNavigate();
+    const { state } = useLocation();
+
+    if (state) {
+        navigate(state);
+    } else {
+        navigate("/");
+    }
+
+    return (
+        <div>
+            <LoginForm />
+            <SignUpFrom />
+        </div>
+    );
 };
 
 export default LoginPage;
