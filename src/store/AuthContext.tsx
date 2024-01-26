368029a62b8e19275d9270e85ca5de9361d7a024
gyojinnK <ksmug1998@gmail.com>
2024-01-26 20:40:08 +0900

    :memo: Add context
    로그인 상태 관리를 위한 AuthContext

---
+++ src/store/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@/firebase";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext<User | null>(null);
+
+export const AuthContextProvider = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
+    const [user, setUser] = useState<User | null>(null);
+
+    const onChange = (user: User | null) => {
+        if (user) {
+            setUser(user);
+        } else {
+            setUser(user);
+        }
+    };
+    useEffect(() => {
+        const subscribe = onAuthStateChanged(auth, onChange);
+        return subscribe;
+    }, []);
+
+    return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
