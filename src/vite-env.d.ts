e3a7c831e8f0d47105898a680e16e8766f32a77d
gyojinnK <ksmug1998@gmail.com>
2024-01-26 20:36:18 +0900

    :label: Add type
    AuthConext 타입 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,3 +7,7 @@ interface AuthContextType {
         setEmail: React.Dispatch<React.SetStateAction<string>>;
     };
 }
+
+type AuthState =
+    | { state: "loaded"; isAuth: boolean; user: User | null }
+    | { state: "error"; error: Error };
