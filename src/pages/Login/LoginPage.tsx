f06c2320c3134e338d144a3763111e483885c467
gyojinnK <ksmug1998@gmail.com>
2024-01-26 13:33:45 +0900

    :lipstick: Add style
    페이지 레이아웃 및 스타일링

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,21 +1,35 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import LoginForm from "./LoginForm";
-import SignUpFrom from "./SignUpForm";
+import { useState } from "react";
+import LoginForm from "../../components/authFrom/LoginForm";
+import SignUpFrom from "../../components/authFrom/SignUpForm";
+import symbol from "@/assets/image/ArNook_symbol.png";
 
 const LoginPage: React.FC = () => {
-    const navigate = useNavigate();
-    const { state } = useLocation();
+    const [isSignUp, setIsSignUp] = useState<boolean>(false);
 
-    if (state) {
-        navigate(state);
-    } else {
-        navigate("/");
-    }
+    const signUpClickHandler = () => {
+        setIsSignUp((prev) => {
+            return prev ? false : true;
+        });
+    };
 
     return (
-        <div>
-            <LoginForm />
-            <SignUpFrom />
+        <div className="h-screen flex justify-center items-center">
+            <div className="sm:flex relative flex-col h-screen mx-20 justify-center items-center hidden">
+                <div className="self-start w-80 space-y-2 absolute left-8 text-stone-500 mix-blend-color-burn">
+                    <p className="text-4xl">나만의</p>
+                    <p className="text-7xl text-center font-['Baumans']">
+                        ArNook한
+                    </p>
+                    <p className="text-3xl text-right">저장소</p>
+                </div>
+                <img src={symbol} className="w-96" />
+                {/* <dd className="font-['Baumans'] text-5xl">ArNook</dd> */}
+            </div>
+            {isSignUp ? (
+                <SignUpFrom onSignUpClick={signUpClickHandler} />
+            ) : (
+                <LoginForm onSignUpClick={signUpClickHandler} />
+            )}
         </div>
     );
 };
