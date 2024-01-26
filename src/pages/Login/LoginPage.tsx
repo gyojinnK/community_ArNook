079723bab1dcfc06623c509b2ec27911440c08e2
gyojinnK <ksmug1998@gmail.com>
2024-01-26 20:33:50 +0900

    :lipstick: Update style
    컨테이너 태그 너비 변경 및 스타일 수정

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -15,7 +15,7 @@ const LoginPage: React.FC = () => {
     return (
         <div className="h-screen flex justify-center items-center">
             <div className="sm:flex relative flex-col h-screen mx-20 justify-center items-center hidden">
-                <div className="self-start w-80 space-y-2 absolute left-8 text-stone-500 mix-blend-color-burn">
+                <div className="self-start w-80 space-y-2 absolute left-8 text-white/70">
                     <p className="text-4xl">나만의</p>
                     <p className="text-7xl text-center font-['Baumans']">
                         ArNook한
