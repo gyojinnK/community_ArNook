6f0f2270ae05fc11fc4c552970f0d908664df129
gyojinnK <ksmug1998@gmail.com>
2024-01-26 20:27:05 +0900

    :memo: Update sorce code
    사용자 인증 유지 상태에 따른 navigate

--- src/Routers/PublicLayout.tsx
+++ src/Routers/PublicLayout.tsx
@@ -1,6 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { AuthContext } from "@/store/AuthContext";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const PublicLayout = () => {
+    const authCtx = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log("인증 유지 상태: ", authCtx);
+        if (authCtx) {
+            navigate("/main");
+        } else {
+            navigate("/");
+        }
+    }, [authCtx]);
+
     return (
         <div>
             <Outlet />
