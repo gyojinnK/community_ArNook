e6ad99af6fae6ff96523992e0e90b6c937bb1a28
gyojinnK <ksmug1998@gmail.com>
2024-01-27 02:04:42 +0900

    :memo: Update file
    요소 컴포넌트 추가 및 배치

--- src/pages/Detail/DetailPage.tsx
+++ src/pages/Detail/DetailPage.tsx
@@ -1,5 +1,17 @@
+import Nav from "@/components/layout/Nav";
+import SideBar from "@/components/layout/SideBar";
+import UserInfo from "@/components/userinfo/UserInfo";
+
 const DetailPage = () => {
-    return <div>DetailPage</div>;
+    return (
+        <>
+            <Nav></Nav>
+            <div className="flex">
+                <SideBar />
+                <UserInfo />
+            </div>
+        </>
+    );
 };
 
 export default DetailPage;
