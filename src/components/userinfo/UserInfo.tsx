1eb287bcf82fa5853155f1549020a9e6668d294a
gyojinnK <ksmug1998@gmail.com>
2024-01-28 13:56:26 +0900

    :memo: Update UserInfo
    레이아웃 구성 변경 및 현재 인증 사용자 프로필 정보 게시

--- src/components/userinfo/UserInfo.tsx
+++ src/components/userinfo/UserInfo.tsx
@@ -1,30 +1,29 @@
 import { Avatar, AvatarImage } from "../ui/avatar";
 import demoImg from "@/assets/image/ArNook_symbol.png";
-import { Button } from "../ui/button";
-import { Pencil1Icon, Pencil2Icon } from "@radix-ui/react-icons";
+
+import UserInfoUpdate from "./UserInfoUpdate";
+import { useContext } from "react";
+import { UserInfoContext } from "@/store/userInfoContext";
+import { UserProfileContext } from "@/store/UserProfileContext";
 
 const UserInfo = () => {
+    const curUser = useContext(UserInfoContext);
+    const curProfile = useContext(UserProfileContext);
+
     return (
         <div className="w-screen h-96 flex items-center">
             <Avatar className="w-72 h-72 ml-24 mr-16">
-                <AvatarImage src={demoImg} />
+                <AvatarImage src={curProfile ? curProfile : demoImg} />
             </Avatar>
             <div className="h-64 self-center flex items-start justify-center flex-col">
-                <p className="text-6xl mb-5">{"UserName"}</p>
+                <p className="text-6xl mb-5">{curUser?.nickName}</p>
                 <div className="flex space-x-10 text-3xl mb-12 text-stone-600">
                     <p>팔로워: {"1234"}</p>
                     <p>팔로잉: {"1234"}</p>
                 </div>
-                <span className="text-xl">
-                    {"안녕하세요 소개글 소개글 테스트 글 입니다."}
-                </span>
+                <span className="text-xl">{curUser?.greet}</span>
             </div>
-            <div className="w-32 mx-auto">
-                <Button className="">
-                    <Pencil2Icon className="mr-2" />
-                    프로필 수정
-                </Button>
-            </div>
+            <UserInfoUpdate />
         </div>
     );
 };
