481ad7076f348a861993c67123810fec899cc583
gyojinnK <ksmug1998@gmail.com>
2024-01-28 13:53:37 +0900

    :adhesive_bandage: Simple fix
    import 구문 수정

--- src/components/authFrom/LoginForm.tsx
+++ src/components/authFrom/LoginForm.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { auth } from "@/firebase";
+import { auth } from "@/firebase/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
