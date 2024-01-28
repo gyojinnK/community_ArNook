b9089abc20ab5bcc8519275cf55ba5531b48bca3
gyojinnK <ksmug1998@gmail.com>
2024-01-28 13:52:26 +0900

    :memo: Update Router
    Context Provider 컴포넌트 추가

--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -7,22 +7,40 @@ import PostingPage from "@/pages/Post/PostingPage";
 import PrivateLayout from "./PrivateLayout";
 import PublicLayout from "./PublicLayout";
 import { AuthContextProvider } from "@/store/AuthContext";
+import { UserInfoProvider } from "@/store/userInfoContext";
+import { UserProfileProvier } from "@/store/UserProfileContext";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <AuthContextProvider>
-                <Routes>
-                    <Route element={<PublicLayout />}>
-                        <Route path="/" element={<LoginPage />} />
-                    </Route>
-                    <Route element={<PrivateLayout />}>
-                        <Route path="/main" element={<MainPage />}></Route>
-                        <Route path="/usersView" element={<UsersViewPage />} />
-                        <Route path="/detail" element={<DetailPage />} />
-                        <Route path="/posting" element={<PostingPage />} />
-                    </Route>
-                </Routes>
+                <UserInfoProvider>
+                    <UserProfileProvier>
+                        <Routes>
+                            <Route element={<PublicLayout />}>
+                                <Route path="/" element={<LoginPage />} />
+                            </Route>
+                            <Route element={<PrivateLayout />}>
+                                <Route
+                                    path="/main"
+                                    element={<MainPage />}
+                                ></Route>
+                                <Route
+                                    path="/usersView"
+                                    element={<UsersViewPage />}
+                                />
+                                <Route
+                                    path="/detail"
+                                    element={<DetailPage />}
+                                />
+                                <Route
+                                    path="/posting"
+                                    element={<PostingPage />}
+                                />
+                            </Route>
+                        </Routes>
+                    </UserProfileProvier>
+                </UserInfoProvider>
             </AuthContextProvider>
         </BrowserRouter>
     );
