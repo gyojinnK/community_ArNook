71f01b5bd15d53fa4a333334dcafbfd34a886b10
gyojinnK <ksmug1998@gmail.com>
2024-01-28 14:03:09 +0900

    :memo: Add folder
    firebase 관련 파일을 관리할 폴더 생성

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSN9XzO1FNMEyK2jIJMQUzHHa4tMUg14M",
+    authDomain: "arnook-381bb.firebaseapp.com",
+    projectId: "arnook-381bb",
+    storageBucket: "arnook-381bb.appspot.com",
+    messagingSenderId: "810789038557",
+    appId: "1:810789038557:web:274711dc31eae1fd852e34",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
