f408c677adcf9fb04b096572977cdd0efb6d8544
gyojinnK <ksmug1998@gmail.com>
2024-01-29 10:53:18 +0900

    :memo: Add file
    layout 컴포넌트의 children UI 추가

---
+++ src/components/layout/ContentBox.tsx
@@ -0,0 +1,5 @@
+const ContentBox: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return <div className="w-full mt-20 ml-20 xl:ml-56">{children}</div>;
+};
+
+export default ContentBox;
