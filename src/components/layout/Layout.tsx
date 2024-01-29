ea8aa2d58ead93819660ecc8cc352a8817d4403f
gyojinnK <ksmug1998@gmail.com>
2024-01-30 00:58:48 +0900

    :memo: Update file
    비밀번호 변경 click event 관련 state, handler 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import ContentBox from "./ContentBox";
 import Nav from "./Nav";
 import SideBar from "./SideBar";
+import UpdatePwForm from "./UpdatePwForm";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [isOpenPw, setIsOpenPw] = useState<boolean>(false);
+
+    const clickIsOpenPwHandler = () => {
+        setIsOpenPw(() => {
+            return isOpenPw ? false : true;
+        });
+    };
+
     return (
-        <>
-            <Nav />
+        <div className="w-full h-full relative">
+            <Nav onSetIsOpenPw={clickIsOpenPwHandler} />
             <div className="flex w-full">
                 <SideBar />
+                <div className="mt-20 h-screen w-2/4 lg:w-1/4"></div>
                 <ContentBox>{children}</ContentBox>
             </div>
-        </>
+            {isOpenPw ? (
+                <UpdatePwForm onSetIsOpenPw={clickIsOpenPwHandler} />
+            ) : null}
+        </div>
     );
 };
 
