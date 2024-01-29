d51beab2dbc53b33028ef3c5fd4671912de0d2fa
gyojinnK <ksmug1998@gmail.com>
2024-01-30 01:04:54 +0900

    :memo: Update file
    useNavigate에 데이터를 담아서 이동하는 코드 추가

--- src/components/usersViewEls/UserCard.tsx
+++ src/components/usersViewEls/UserCard.tsx
@@ -4,12 +4,14 @@ import { getDownloadURL, ref } from "firebase/storage";
 import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const UserCard: React.FC<{ imgName: string }> = (props) => {
     const [imgPath, setImgPath] = useState<string>();
     const [nickName, setNickName] = useState<string>("");
     const imgRef = ref(storage, `profile/${props.imgName}`);
     const dbRef = doc(db, "users", props.imgName);
+    const navigate = useNavigate();
 
     useEffect(() => {
         getDownloadURL(imgRef).then((path) => {
@@ -22,8 +24,21 @@ const UserCard: React.FC<{ imgName: string }> = (props) => {
         });
     }, []);
 
+    const navigateMyPageHandler = () => {
+        navigate("/otherDetail", {
+            state: {
+                uEmail: props.imgName,
+                uNickname: nickName,
+                imgPath: imgPath,
+            },
+        });
+    };
+
     return (
-        <Card className="inline-block w-64 h-80 mx-4 mt-8 rounded-2xl xl:w-80 xl:h-96  bg-stone-100">
+        <Card
+            onClick={navigateMyPageHandler}
+            className="inline-block w-64 h-80 mx-4 mt-8 rounded-2xl xl:w-80 xl:h-96  bg-stone-100 focus: cursor-pointer hover:bg-stone-200"
+        >
             <div className="flex flex-col items-center">
                 <Avatar className="w-40 h-40 xl:w-48 xl:h-48 mt-5 mb-3">
                     <AvatarImage src={imgPath} />
