1e7d037f4ae9183c57fb7fb4b62de22413f44ea3
gyojinnK <ksmug1998@gmail.com>
2024-01-30 00:59:56 +0900

    :lipstick: Update style
    비밀번호 변경 인터페이스 추가 및 props 추가

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,18 +1,18 @@
 import { Button } from "../ui/button";
 import NavDropdownBox from "./NavDropdownBox";
 
-const Nav: React.FC = () => {
+const Nav: React.FC<{ onSetIsOpenPw: () => void }> = (props) => {
     return (
-        <div className="fixed w-full">
+        <div className="fixed w-full z-40">
             <div className="flex justify-between items-center h-20 border-b bg-popover shadow-sm">
-                <div className="font-['Baumans'] text-4xl mx-14 text-stone-700">
+                <div className="font-['Baumans'] text-4xl mx-10 text-stone-700">
                     ArNook
                 </div>
-                <div className="mx-10 flex justify-evenly w-40">
+                <div className="mx-6 flex justify-evenly w-40">
                     <Button className="self-center bg-stone-700">
                         + 게시물
                     </Button>
-                    <NavDropdownBox />
+                    <NavDropdownBox onSetIsOpenPw={props.onSetIsOpenPw} />
                 </div>
             </div>
         </div>
