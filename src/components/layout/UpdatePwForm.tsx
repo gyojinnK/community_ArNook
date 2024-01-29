ea0f2351c6a7ae36c4d31dfda07b7c929d867e14
gyojinnK <ksmug1998@gmail.com>
2024-01-30 01:07:45 +0900

    :memo: Add file
    비밀번호 폼 컴포넌트 추가

---
+++ src/components/layout/UpdatePwForm.tsx
@@ -0,0 +1,110 @@
+import { FormEvent, useContext, useState } from "react";
+import { Button } from "../ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { AuthContext } from "@/store/AuthContext";
+import { db } from "@/firebase/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { updatePassword } from "firebase/auth";
+
+// zod로 다시하기
+const UpdatePwForm: React.FC<{ onSetIsOpenPw: () => void }> = (props) => {
+    const [enteredCurPassword, setEnteredCurPassword] = useState<string>("");
+    const [enteredNewPassword, setEnteredNewPassword] = useState<string>("");
+    const curUser = useContext(AuthContext);
+    const dbRef = doc(db, "users", curUser?.email);
+
+    const changeCurPassword = (e: FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = e;
+        setEnteredCurPassword(value);
+    };
+    const changeNewPassword = (e: FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = e;
+        setEnteredNewPassword(value);
+    };
+    const fetchPassword = async () => {
+        const snapshot = await getDoc(dbRef);
+        if (snapshot) {
+            return snapshot.data()?.password;
+        }
+    };
+
+    const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const curUserPassword = fetchPassword;
+            if (String(curUserPassword) === enteredCurPassword && curUser) {
+                updatePassword(curUser, enteredNewPassword)
+                    .then(() => {
+                        console.log("Successful!");
+                    })
+                    .catch((error: Error) => {
+                        error.stack;
+                    });
+            }
+        } catch (eror) {
+            console.log("Update Error");
+            props.onSetIsOpenPw;
+        }
+    };
+
+    return (
+        <div
+            // onClick={props.onSetIsOpenPw}
+            className="fixed flex justify-center items-center top-0 left-0 w-full h-full z-50 bg-black/80"
+        >
+            <Card className="w-96">
+                <CardHeader>
+                    <CardTitle>비밀번호 변경</CardTitle>
+                    <CardDescription>
+                        새로운 비밀번호를 설정해주세요.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={onSubmitHandler}>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="curPw">현재 비밀번호</Label>
+                                <Input
+                                    type="password"
+                                    id="curPw"
+                                    value={enteredCurPassword}
+                                    onChange={changeCurPassword}
+                                />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="newPw">새로운 비밀번호</Label>
+                                <Input
+                                    type="password"
+                                    id="newPw"
+                                    value={enteredNewPassword}
+                                    onChange={changeNewPassword}
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button variant="outline" onClick={props.onSetIsOpenPw}>
+                        취소하기
+                    </Button>
+                    <Button type="submit">변경하기</Button>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
+
+export default UpdatePwForm;
