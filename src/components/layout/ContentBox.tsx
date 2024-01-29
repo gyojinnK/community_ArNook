5041d12197747e7d5073cd2e28e6a1b3f6603dfd
gyojinnK <ksmug1998@gmail.com>
2024-01-30 00:57:43 +0900

    :adhesive_bandage: Simple fix
    return문 수정

--- src/components/layout/ContentBox.tsx
+++ src/components/layout/ContentBox.tsx
@@ -1,5 +1,5 @@
 const ContentBox: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return <div className="w-full mt-20 ml-20 xl:ml-56">{children}</div>;
+    return <div className="w-10/12 lg:w-full mt-20">{children}</div>;
 };
 
 export default ContentBox;
