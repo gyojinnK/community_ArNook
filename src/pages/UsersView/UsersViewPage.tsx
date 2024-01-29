5b9b98544bf351ca6941e9cbd5f212a44aed4148
gyojinnK <ksmug1998@gmail.com>
2024-01-29 10:47:47 +0900

    :lipstick: Add layout ui
    layout 컴포넌트 재사용

--- src/pages/UsersView/UsersViewPage.tsx
+++ src/pages/UsersView/UsersViewPage.tsx
@@ -1,5 +1,12 @@
+import Layout from "@/components/layout/Layout";
+import UsersViewWrap from "@/components/usersViewEls/UsersViewWrap";
+
 const UsersViewPage = () => {
-    return <div>UsersViewPage</div>;
+    return (
+        <Layout>
+            <UsersViewWrap />
+        </Layout>
+    );
 };
 
 export default UsersViewPage;
