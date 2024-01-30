9901028f8ba8fad99684e119196ce67e39d98bb2
gyojinnK <ksmug1998@gmail.com>
2024-01-30 17:26:48 +0900

    :children_crossing: Improve user experience / usability.
    기존 useEffect로 수행했던 인증 상태 검증 후 navigate를 인증된 사용자가 로그인 페이지를 거치지 않고 메인 페이지에 바로 접근하게 하기 위해서 useLayoutEffect로 변경

--- src/Routers/PrivateLayout.tsx
+++ src/Routers/PrivateLayout.tsx
@@ -1,5 +1,5 @@
 import { AuthContext } from "@/store/AuthContext";
-import { useContext, useEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const PrivateLayout = () => {
@@ -7,8 +7,7 @@ const PrivateLayout = () => {
 
     const authCtx = useContext(AuthContext);
 
-    useEffect(() => {
-        // 레이아웃
+    useLayoutEffect(() => {
         console.log("인증 유지 상태: ", authCtx);
         if (authCtx) {
             navigate("/main");
--- src/Routers/PublicLayout.tsx
+++ src/Routers/PublicLayout.tsx
@@ -1,12 +1,12 @@
 import { AuthContext } from "@/store/AuthContext";
-import { useContext, useEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const PublicLayout = () => {
     const authCtx = useContext(AuthContext);
     const navigate = useNavigate();
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         console.log("인증 유지 상태: ", authCtx);
         if (authCtx) {
             navigate("/main");
