c8b57ebc05d9bfc9a4d905b1d446ef5cf4fed1d7
gyojinnK <ksmug1998@gmail.com>
2024-02-01 00:20:32 +0900

    :memo: Update file
    팔로워, 팔로잉 인터페이스 추가

--- src/components/otherDetailEls/OtherUserInfo.tsx
+++ src/components/otherDetailEls/OtherUserInfo.tsx
@@ -1,41 +1,60 @@
 import { useLocation } from "react-router-dom";
 import React, { Suspense, useEffect, useState } from "react";
-import { db } from "@/firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { getDBRef } from "@/firebase/firebase";
+import { onSnapshot } from "firebase/firestore";
+import FollowerBox from "../followEls/followerBox";
+import { Button } from "../ui/button";
 
 const UserAvatar = React.lazy(() => import("../detailEls/UserAvatar"));
 
 const OtherUserInfo: React.FC = () => {
     const [greet, setGreet] = useState<string>("");
+    const [followerCount, setFollowerCount] = useState<number>(0);
+    const [followingCount, setFollowingCount] = useState<number>(0);
     const loc = useLocation();
-    const dbRef = doc(db, "users", loc?.state.uEmail);
+    const dbRef = getDBRef(loc?.state.uEmail);
 
     useEffect(() => {
         const fetchData = async () => {
-            const snapshot = await getDoc(dbRef);
-            setGreet(snapshot.data()?.greet);
+            onSnapshot(dbRef!, (snapshot) => {
+                setGreet(snapshot.data()?.greet);
+                setFollowerCount(snapshot.data()?.follower.length);
+                setFollowingCount(snapshot.data()?.following.length);
+            });
         };
         fetchData();
     }, []);
 
     return (
-        <div className="flex relative justify-between items-center flex-col lg:flex-row w-full">
-            <div className="flex flex-col lg:flex-row">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <UserAvatar curPImageId={loc?.state.imgPath} />
-                </Suspense>
-                <div className="mx-5 self-center flex items-start justify-center flex-col">
-                    <p className="text-3xl mb-5 xl:text-6xl">
-                        {loc?.state.uNickname}
-                    </p>
-                    <div className="flex space-x-10 text-xl xl:text-3xl xl:mb-12 mb-6 text-stone-600">
-                        <p>팔로워: {"1234"}</p>
-                        <p>팔로잉: {"1234"}</p>
+        <>
+            <div className="flex relative justify-between items-center flex-col lg:flex-row w-full">
+                <div className="flex flex-col lg:flex-row">
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <UserAvatar curPImageId={loc?.state.imgPath} />
+                    </Suspense>
+                    <div className="mx-5 self-center flex items-start justify-center flex-col">
+                        <p className="text-3xl xl:text-6xl">
+                            {loc?.state.uNickname}
+                        </p>
+                        <div className=" my-4">{loc?.state.uEmail}</div>
+                        <div className="flex space-x-2 text-xl xl:text-3xl xl:mb-12 mb-6 text-stone-600">
+                            <Button className="bg-white text-stone-600 border border-stone-600 hover:bg-white focus: cursor-default">
+                                팔로잉 {followingCount}
+                            </Button>
+                            <Button className="bg-white text-stone-600 border border-stone-600 hover:bg-white focus: cursor-default">
+                                팔로워 {followerCount}
+                            </Button>
+                        </div>
+                        <span className="text-base xl:text-xl">{greet}</span>
                     </div>
-                    <span className="text-base xl:text-xl">{greet}</span>
                 </div>
+                <FollowerBox
+                    otherUserEmail={loc?.state.uEmail}
+                    otherUserNickname={loc?.state.uNickname}
+                />
             </div>
-        </div>
+            <hr className="text-popover w-5/6 mx-auto shadow-sm" />
+        </>
     );
 };
 
