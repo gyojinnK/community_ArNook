61dcbeba0949915c1e51e4f484f0249349433eb4
gyojinnK <ksmug1998@gmail.com>
2024-02-01 00:14:56 +0900

    :adhesive_bandage: Simple fix
    테스트 log 코드 삭제

--- src/Routers/PrivateLayout.tsx
+++ src/Routers/PrivateLayout.tsx
@@ -8,7 +8,6 @@ const PrivateLayout = () => {
     const authCtx = useContext(AuthContext);
 
     useLayoutEffect(() => {
-        console.log("인증 유지 상태: ", authCtx);
         if (authCtx) {
             navigate("/main");
         } else {
--- src/Routers/PublicLayout.tsx
+++ src/Routers/PublicLayout.tsx
@@ -7,7 +7,6 @@ const PublicLayout = () => {
     const navigate = useNavigate();
 
     useLayoutEffect(() => {
-        console.log("인증 유지 상태: ", authCtx);
         if (authCtx) {
             navigate("/main");
         } else {
