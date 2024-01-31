12332d58b05502c5a1f748fa912da0b189f4e977
gyojinnK <ksmug1998@gmail.com>
2024-02-01 00:26:23 +0900

    :memo: Add file
    팔로워, 팔로잉 인터페이스 컴포넌트 생성 및 개발

---
+++ src/components/detailEls/FollowerInfo.tsx
@@ -0,0 +1,93 @@
+import { useContext, useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { getDoc } from "firebase/firestore";
+import { AuthContext } from "@/store/AuthContext";
+import { getDBRef } from "@/firebase/firebase";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog";
+import { ScrollArea } from "../ui/scroll-area";
+import FollowerBox from "../followEls/followerBox";
+import { Separator } from "../ui/separator";
+
+const FollowerInfo = () => {
+    const curUser = useContext(AuthContext);
+    const docRef = getDBRef(curUser?.email!);
+    const [FollowerDatas, setFollowerDatas] = useState<
+        { email: string; nickname: string }[]
+    >([]);
+    const [followerCount, setFollowerCount] = useState<number>(0);
+    const [cheker, setChecker] = useState<boolean>(false);
+
+    useEffect(() => {
+        getDoc(docRef)
+            .then((ss) => {
+                setFollowerDatas(ss.data()?.follower);
+            })
+            .then(() => setFollowerCount(FollowerDatas.length));
+    }, [cheker]);
+
+    useEffect(() => {
+        setFollowerCount(FollowerDatas.length);
+    }, [FollowerDatas]);
+
+    const followListUpdateHandler = () => {
+        setChecker((prev) => {
+            return prev ? false : true;
+        });
+    };
+
+    // return <Button variant="outline">팔로워 {followerCount}</Button>;
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline" onClick={followListUpdateHandler}>
+                    팔로워 {followerCount}
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>팔로워한 유저</DialogTitle>
+                </DialogHeader>
+                <ScrollArea className="h-56 w-full rounded-md border">
+                    {FollowerDatas.length !== 0 ? (
+                        FollowerDatas.map((follower) => {
+                            return (
+                                <>
+                                    <div
+                                        className="flex justify-between items-center"
+                                        onClick={followListUpdateHandler}
+                                    >
+                                        <h4 className="mx-8 text-md font-medium leading-none">
+                                            {follower.email}
+                                        </h4>
+                                        <FollowerBox
+                                            key={Math.random()}
+                                            otherUserEmail={follower.email}
+                                            otherUserNickname={
+                                                follower.nickname
+                                            }
+                                        />
+                                    </div>
+                                    <Separator />
+                                </>
+                            );
+                        })
+                    ) : (
+                        <div className="my-10 mx-10 text-stone-500">
+                            팔로워가 없습니다...
+                        </div>
+                    )}
+                </ScrollArea>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+// 팔로워 리스트 보여줘야함
+
+export default FollowerInfo;
---
+++ src/components/detailEls/FollowingInfo.tsx
@@ -0,0 +1,94 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { ScrollArea } from "../ui/scroll-area";
+import FollowerBox from "../followEls/followerBox";
+import { getDBRef } from "@/firebase/firebase";
+import { AuthContext } from "@/store/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import { getDoc } from "firebase/firestore";
+import { Separator } from "../ui/separator";
+
+const FollowingInfo: React.FC = () => {
+    const curUser = useContext(AuthContext);
+    const docRef = getDBRef(curUser?.email!);
+    const [FollowingDatas, setFollowingDatas] = useState<
+        { email: string; nickname: string }[]
+    >([]);
+    const [cheker, setChecker] = useState<boolean>(false);
+    const [followingCount, setFollowingCount] = useState<number>(0);
+
+    // useEffect(() => {
+    //     getDoc(docRef).then((ss) => {
+    //         setFollowingDatas(ss.data()?.following);
+    //     });
+    // }, []);
+
+    useEffect(() => {
+        getDoc(docRef).then((ss) => {
+            setFollowingDatas(ss.data()?.following);
+        });
+    }, [cheker]);
+
+    useEffect(() => {
+        setFollowingCount(FollowingDatas.length);
+    }, [FollowingDatas]);
+
+    const followListUpdateHandler = () => {
+        setChecker((prev) => {
+            return prev ? false : true;
+        });
+    };
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline" onClick={followListUpdateHandler}>
+                    팔로잉 {followingCount}
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>팔로잉한 유저</DialogTitle>
+                </DialogHeader>
+                <ScrollArea className="h-56 w-full rounded-md border">
+                    {FollowingDatas.length !== 0 ? (
+                        FollowingDatas.map((following) => {
+                            return (
+                                <>
+                                    <div
+                                        className="flex justify-between items-center"
+                                        onClick={followListUpdateHandler}
+                                    >
+                                        <h4 className="mx-8 text-md font-medium leading-none">
+                                            {following.email}
+                                        </h4>
+                                        <FollowerBox
+                                            key={Math.random()}
+                                            otherUserEmail={following.email}
+                                            otherUserNickname={
+                                                following.nickname
+                                            }
+                                        />
+                                    </div>
+                                    <Separator />
+                                </>
+                            );
+                        })
+                    ) : (
+                        <div className="my-10 mx-10 text-stone-500">
+                            팔로잉이 없습니다...
+                        </div>
+                    )}
+                </ScrollArea>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default FollowingInfo;
