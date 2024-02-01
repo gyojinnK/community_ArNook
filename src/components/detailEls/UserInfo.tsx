925b94b10974f3732ed2dc7bbfe59806a379c25e
gyojinnK <ksmug1998@gmail.com>
2024-02-02 02:37:20 +0900

    :memo: Update file
    사용자가 업로드한 게시물 리스트 뷰어 개발

--- src/components/detailEls/UserInfo.tsx
+++ src/components/detailEls/UserInfo.tsx
@@ -5,8 +5,11 @@ import { UserProfileContext } from "@/store/UserProfileContext";
 import UserAvatar from "./UserAvatar";
 import FollowingInfo from "./FollowingInfo";
 import FollowerInfo from "./FollowerInfo";
+import UserPostView from "./UserPostView";
+import { AuthContext } from "@/store/AuthContext";
 
 const UserInfo = () => {
+    const curAuthUser = useContext(AuthContext);
     const curUser = useContext(UserInfoContext);
     const curProfile = useContext(UserProfileContext);
 
@@ -24,8 +27,11 @@ const UserInfo = () => {
                 <div className="flex flex-col lg:flex-row">
                     <UserAvatar curPImageId={curPImageId} />
                     <div className=" mx-5 self-center flex items-start justify-center flex-col">
-                        <p className="text-3xl mb-5 xl:text-6xl">
+                        <p className="text-3xl xl:text-6xl">
                             {curUser?.nickName}
+                        </p>
+                        <p className="text-sm ml-2 mb-5 text-stone-400">
+                            {curAuthUser?.email}
                         </p>
                         <div className="flex space-x-2 text-xl xl:text-3xl xl:mb-12 mb-6 text-stone-600">
                             <FollowingInfo />
@@ -39,6 +45,7 @@ const UserInfo = () => {
                 <UserInfoUpdate onChangePImgHandler={changePImgHandler} />
             </div>
             <hr className="text-popover w-5/6 mx-auto shadow-sm" />
+            <UserPostView email={curAuthUser?.email} />
         </>
     );
 };
