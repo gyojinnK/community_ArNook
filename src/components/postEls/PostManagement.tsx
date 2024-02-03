dc8e8f19f66d37cb1e177b3a9e9df7d12375a1bd
gyojinnK <ksmug1998@gmail.com>
2024-02-03 22:29:23 +0900

    :memo: Add file
    게시물 클릭 시 수정, 삭제 인터페이스 제공

---
+++ src/components/postEls/PostManagement.tsx
@@ -0,0 +1,50 @@
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+import PostUpdateDialog from "./PostUpdateDialog";
+import { Dispatch } from "react";
+import { Timestamp } from "firebase/firestore";
+import PostRemoveDialog from "./PostRemoveDialog";
+
+const PostManagement: React.FC<{
+    onClose: React.Dispatch<React.SetStateAction<boolean>>;
+    email: string;
+    postId: string;
+    postTitle: string;
+    postHashtags: string[];
+    postContent: string;
+    createdAt: Timestamp;
+    postImgUrl: string;
+}> = (props) => {
+    const closeHandler = () => {
+        props.onClose(false);
+    };
+    return (
+        <div className="w-full h-full bg-black/80 absolute top-0 left-0 z-30 flex justify-center items-center">
+            <Cross1Icon
+                onClick={closeHandler}
+                className="absolute top-5 right-5 text-white"
+            />
+            <PostUpdateDialog
+                email={props.email}
+                postId={props.postId}
+                postTitle={props.postTitle}
+                postHashtags={props.postHashtags}
+                postContent={props.postContent}
+                createdAt={props.createdAt}
+                postImgUrl={props.postImgUrl}
+            />
+            <PostRemoveDialog
+                onClose={props.onClose}
+                email={props.email}
+                postId={props.postId}
+                postTitle={props.postTitle}
+                postHashtags={props.postHashtags}
+                postContent={props.postContent}
+                createdAt={props.createdAt}
+                postImgUrl={props.postImgUrl}
+            />
+        </div>
+    );
+};
+
+export default PostManagement;
