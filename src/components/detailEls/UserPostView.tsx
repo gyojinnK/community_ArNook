5b9f0e18e853f509b6e9dadd93d7a93a39186135
gyojinnK <ksmug1998@gmail.com>
2024-02-03 22:13:59 +0900

    :memo: Update file
    게시물 리스트를 생성일자를 기준으로 내림차순으로 정렬

--- src/components/detailEls/UserPostView.tsx
+++ src/components/detailEls/UserPostView.tsx
@@ -1,8 +1,10 @@
 import { collection, onSnapshot, query } from "firebase/firestore";
 import PostCard from "../postEls/PostCard";
-import { db } from "@/firebase/firebase";
+import { db, getFeedStorageRef } from "@/firebase/firebase";
 import { useEffect, useState } from "react";
 import { Post } from "@/vite-env";
+import { get } from "lodash";
+import { getDownloadURL } from "firebase/storage";
 
 const UserPostView: React.FC<{ email: string | null | undefined }> = (
     props
@@ -17,7 +19,6 @@ const UserPostView: React.FC<{ email: string | null | undefined }> = (
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             let tempArr: Post[] = [];
             querySnapshot.forEach((post) => {
-                console.log(post.data());
                 const splits = post.data().postId.split("|");
                 // setPostOwner();
                 if (splits[0] === props.email) {
@@ -26,6 +27,7 @@ const UserPostView: React.FC<{ email: string | null | undefined }> = (
                         postId: splits[1],
                         email: post.data().email,
                         postTitle: post.data().postTitle,
+                        postHashtags: post.data().postHashtags,
                         postContent: post.data().postContent,
                         likeCount: post.data().likeCount,
                         commentCount: post.data().commentCount,
@@ -36,18 +38,26 @@ const UserPostView: React.FC<{ email: string | null | undefined }> = (
                 }
             });
             if (tempArr) {
+                tempArr.sort(
+                    (a: Post, b: Post) =>
+                        b.createdAt.toDate().getTime() -
+                        a.createdAt.toDate().getTime()
+                );
                 setPostList(tempArr);
             }
         });
         return () => unsubscribe();
     }, [postOwner]);
 
-    console.log(postList);
     return (
         <div className="w-full text-center">
             {postList.map((postInfo: Post) => (
                 <PostCard
+                    key={postInfo.postId}
+                    email={postInfo.email}
+                    postId={postInfo.postId}
                     postTitle={postInfo.postTitle}
+                    postHashtags={postInfo.postHashtags}
                     createdAt={postInfo.createdAt}
                     postContent={postInfo.postContent}
                 ></PostCard>
