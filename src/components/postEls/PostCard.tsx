a0ee9be043198ed3a6c9d167e93bce17d1df0191
gyojinnK <ksmug1998@gmail.com>
2024-02-03 22:23:34 +0900

    :memo: Update file
    테스트 코드 삭제 및 유저 fire store 및 storage 데이터로 동적 게시물 카드 생성

--- src/components/postEls/PostCard.tsx
+++ src/components/postEls/PostCard.tsx
@@ -1,4 +1,4 @@
-import { Timestamp } from "firebase/firestore";
+import { Timestamp, onSnapshot } from "firebase/firestore";
 import {
     Card,
     CardContent,
@@ -7,22 +7,109 @@ import {
     CardHeader,
     CardTitle,
 } from "../ui/card";
+import { getFeedDBRef, getFeedStorageRef } from "@/firebase/firebase";
+import { getDownloadURL } from "firebase/storage";
+import { useContext, useEffect, useState } from "react";
+import { Badge } from "../ui/badge";
+import PostManagement from "./PostManagement";
+import { AuthContext } from "@/store/AuthContext";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { indexOf } from "lodash";
 
 const PostCard: React.FC<{
+    email: string;
+    postId: string;
     postTitle: string;
+    postHashtags: string[];
     createdAt: Timestamp;
     postContent: string;
 }> = (props) => {
+    const [imgUrl, setImgUrl] = useState<string>("");
+    const [isManage, setIsManage] = useState<boolean>(false);
+    const [reload, setReload] = useState<boolean>(false);
+    const curUser = useContext(AuthContext);
+    const formmatedCreateAt = props.createdAt
+        .toDate()
+        .toISOString()
+        .slice(0, 10);
+    const imgRef = getFeedStorageRef(props.email, props.postId);
+
+    useEffect(() => {
+        console.log("아오 확인하자: ", imgRef, "-", props.postId);
+        if (imgRef.name === props.postId) {
+            getDownloadURL(imgRef)
+                .then((imgUrl: string) => {
+                    setImgUrl(imgUrl);
+                })
+                .catch((error) => {
+                    console.log(
+                        "해당 포스트는 이미지가 없습니다!: ",
+                        props.postTitle
+                    );
+                    setReload((prev) => {
+                        return prev ? false : true;
+                    });
+                });
+        }
+    }, [isManage]);
+
+    useEffect(() => {
+        setImgUrl("");
+        console.log("카드 이미지 reloading");
+    }, [reload]);
+
+    const managementClickHandler = () => {
+        setIsManage(true);
+    };
+
     return (
-        <Card className="text-left w-80 h-96 mx-10 my-10 rounded-3xl inline-block bg-stone-100/50">
-            <CardHeader>
-                <CardTitle>{props.postTitle}</CardTitle>
-                <CardDescription>
-                    {props.createdAt.toDate().toUTCString()}
-                </CardDescription>
-            </CardHeader>
-            <CardContent>{props.postContent}</CardContent>
-            <CardFooter></CardFooter>
+        <Card className="text-left w-80 h-96 mx-10 my-10 rounded-3xl inline-block bg-stone-50 overflow-hidden relative">
+            <>
+                {props.email === curUser?.email && isManage ? (
+                    <PostManagement
+                        onClose={setIsManage}
+                        email={props.email}
+                        postId={props.postId}
+                        postTitle={props.postTitle}
+                        postHashtags={props.postHashtags}
+                        postContent={props.postContent}
+                        createdAt={props.createdAt}
+                        postImgUrl={imgUrl}
+                    ></PostManagement>
+                ) : null}
+                {imgUrl ? <img className="h-1/3 w-full" src={imgUrl} /> : null}
+                <CardHeader>
+                    <CardTitle className="flex justify-between items-center">
+                        {props.postTitle}
+                        {props.email === curUser?.email ? (
+                            <Pencil2Icon
+                                onClick={managementClickHandler}
+                                className="w-6 h-6"
+                            />
+                        ) : null}
+                    </CardTitle>
+
+                    <CardDescription>
+                        <div>{props.email}</div>
+                        <div className="mb-2">{formmatedCreateAt}</div>
+                        <div className="w-full">
+                            {props.postHashtags.map((tag, i) => (
+                                <Badge
+                                    key={i}
+                                    variant="outline"
+                                    className="text-stone-400 inline-block "
+                                >
+                                    # {tag}
+                                </Badge>
+                            ))}
+                        </div>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="">{props.postContent}</CardContent>
+                <CardFooter>
+                    <div className="absolute z-10 left-0 bottom-0 w-full h-10 bg-gradient-to-t from-stone-100 to-transparent"></div>
+                </CardFooter>
+            </>
         </Card>
     );
 };
