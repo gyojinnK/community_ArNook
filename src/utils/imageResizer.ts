f7ee7e48fa55152b9892ddc1f71ff0fa3bb56435
gyojinnK <ksmug1998@gmail.com>
2024-02-04 23:34:49 +0900

    :memo: Add folder
    데이터베이스 관련 및 전역에서 자주 사용하는 함수를 모듈화하여 정리하는 폴더 생성

---
+++ src/utils/imageResizer.ts
@@ -0,0 +1,38 @@
+import Resizer from "react-image-file-resizer";
+
+const resizeFile = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        Resizer.imageFileResizer(
+            file,
+            300,
+            300,
+            "JPEG",
+            100,
+            0,
+            (uri) => {
+                if (typeof uri === "string") {
+                    resolve(uri);
+                } else {
+                    reject("The uri is not a string.");
+                }
+            },
+            "base64"
+        );
+    });
+};
+
+export const resizedImgToBlob = async (f: File) => {
+    const resizedImg = await resizeFile(f);
+    const byteCharacters = atob(resizedImg.split(",")[1]);
+    const byteNumbers = Array.from(byteCharacters, (char) =>
+        char.charCodeAt(0)
+    );
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: "image/jpeg" });
+    const file = new File([blob], "image.jpg", {
+        type: "image/jpeg",
+        lastModified: new Date().getTime(),
+    });
+
+    return file;
+};
