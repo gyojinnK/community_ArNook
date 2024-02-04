d786036826945591e46a9cba0e7337e8912b83a5
gyojinnK <ksmug1998@gmail.com>
2024-02-04 23:23:51 +0900

    :memo: Update file
    home 버튼 추가 (전체 게시물 나열 페이지)

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,27 +1,43 @@
 import { useNavigate } from "react-router-dom";
 import { Card } from "../ui/card";
 import group from "@/assets/vector/group.svg";
+import home from "@/assets/vector/home.svg";
 
 const SideBar: React.FC = () => {
     const navigate = useNavigate();
-    const navigateHandler = () => {
+    const navigateMainHandler = () => {
+        navigate("/main");
+    };
+    const navigateUserListHandler = () => {
         navigate("/usersView");
     };
 
     return (
         <>
-            <Card className="fixed mt-20 h-screen w-1/6 lg:w-1/5 rounded-none border-pop">
+            <Card className="fixed mt-20 h-screen w-1/6 lg:w-1/6 rounded-none border-pop">
                 <div className="w-full h-16 flex justify-center items-center">
                     <div
-                        onClick={navigateHandler}
+                        onClick={navigateMainHandler}
                         className="px-2 lg:px-5 lg:py-2 flex justify-between items-center hover:bg-accent rounded-sm focus: cursor-pointer"
                     >
-                        <img src={group} className="w-8" />
-                        <p className="hidden xl:inline-block text-xl mx-2 ">
-                            계정 둘러보기
+                        <img src={home} className="w-8" />
+                        <p className="hidden lg:inline-block text-xl mx-2 ">
+                            Home
                         </p>
                     </div>
                 </div>
+                <div className="w-full h-16 flex justify-center items-center">
+                    <div
+                        onClick={navigateUserListHandler}
+                        className="px-2 lg:px-5 lg:py-2 flex justify-between items-center hover:bg-accent rounded-sm focus: cursor-pointer"
+                    >
+                        <img src={group} className="w-8" />
+                        <p className="hidden lg:inline-block text-xl mx-2 ">
+                            Users
+                        </p>
+                    </div>
+                </div>
+
                 <hr className="w-2/3 mx-auto" />
             </Card>
         </>
