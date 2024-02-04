72a10cead660bc1f86dcc5be61028d435b57652a
gyojinnK <ksmug1998@gmail.com>
2024-02-04 23:20:43 +0900

    :adhesive_bandage: Simple fix
    폴더명 변경에 따른 import문 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
-import { auth } from "./firebase/firebase";
+import { auth } from "./utils/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function App() {
