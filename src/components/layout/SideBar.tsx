353265258991bb1fd16855129f530c3fa91be54d
gyojinnK <ksmug1998@gmail.com>
2024-02-06 19:24:34 +0900

    :memo: Update file
    사이드 메뉴바에 메인 페이지와 마이페이지 navigator 추가

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { Card } from "../ui/card";
 import group from "@/assets/vector/group.svg";
 import home from "@/assets/vector/home.svg";
+import mypage from "@/assets/vector/mypage.svg";
 
 const SideBar: React.FC = () => {
     const navigate = useNavigate();
@@ -11,11 +12,14 @@ const SideBar: React.FC = () => {
     const navigateUserListHandler = () => {
         navigate("/usersView");
     };
+    const navigateMyPageHandler = () => {
+        navigate("/detail");
+    };
 
     return (
         <>
-            <Card className="fixed mt-20 h-screen w-1/6 lg:w-1/6 rounded-none border-pop">
-                <div className="w-full h-16 flex justify-center items-center">
+            <Card className="fixed mt-20 h-screen w-1/6 lg:w-1/6 rounded-none border-pop select-none">
+                <div className="w-full h-16 flex justify-center items-center ">
                     <div
                         onClick={navigateMainHandler}
                         className="px-2 lg:px-5 lg:py-2 flex justify-between items-center hover:bg-accent rounded-sm focus: cursor-pointer"
@@ -34,6 +38,17 @@ const SideBar: React.FC = () => {
                         <img src={group} className="w-8" />
                         <p className="hidden lg:inline-block text-xl mx-2 ">
                             Users
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full h-16 flex justify-center items-center">
+                    <div
+                        onClick={navigateMyPageHandler}
+                        className="px-2 lg:px-5 lg:py-2 flex justify-between items-center hover:bg-accent rounded-sm focus: cursor-pointer"
+                    >
+                        <img src={mypage} className="w-8" />
+                        <p className="hidden lg:inline-block text-md mx-2 ">
+                            My Arnook
                         </p>
                     </div>
                 </div>
