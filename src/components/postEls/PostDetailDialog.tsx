c3ccc16a4e497e1720c63b2061f8687d02669329
gyojinnK <ksmug1998@gmail.com>
2024-02-06 19:42:11 +0900

    :memo: Add file
    게시물 상세보기 컴포넌트 생성 및 개발

---
+++ src/components/postEls/PostDetailDialog.tsx
@@ -0,0 +1,90 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTrigger,
+} from "../ui/dialog";
+import { Badge } from "../ui/badge";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { Link2Icon } from "@radix-ui/react-icons";
+
+const PostDetailDialog: React.FC<{
+    onNavigate: () => void;
+    email: string;
+    postId: string;
+    postTitle: string;
+    postHashtags: string[];
+    createdAt: string;
+    postContent: string;
+    extraLink: string | null;
+    postImgUrl: string | null;
+}> = (props) => {
+    const extraLinkNavigateHandler = () => {
+        window.open(`${props.extraLink}`);
+    };
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <div className="inline-block w-fit text-xs text-stone-500 text-left focus: cursor-pointer hover:text-blue-600 hover:underline ">
+                    자세히 보기 +
+                </div>
+            </DialogTrigger>
+            <DialogContent className="w-full max-h-full overflow-scroll">
+                <DialogHeader>{props.postTitle}</DialogHeader>
+                <DialogDescription>
+                    <div className="flex justify-start items-center">
+                        <div className="mr-2">작성자:</div>
+                        <div
+                            onClick={props.onNavigate}
+                            className="focus: cursor-pointer hover:underline hover:text-blue-700"
+                        >
+                            {props.email}
+                        </div>
+                    </div>
+                    <div>{props.createdAt}</div>
+                </DialogDescription>
+                {props.postImgUrl ? (
+                    <img src={props.postImgUrl} className="w-full h-full" />
+                ) : (
+                    <div>이미지가 없는 게시물 입니다.</div>
+                )}
+                <div className="w-full">
+                    {props.postHashtags.map((tag, i) => (
+                        <Badge
+                            key={i}
+                            variant="outline"
+                            className="text-stone-400 inline-block "
+                        >
+                            # {tag}
+                        </Badge>
+                    ))}
+                </div>
+                <div className="whitespace-pre-wrap">{props.postContent}</div>
+                <DialogFooter className="w-full">
+                    {props.extraLink ? (
+                        <Card
+                            onClick={extraLinkNavigateHandler}
+                            className="mt-5 w-full border-none shadow-none"
+                        >
+                            <CardFooter className="flex w-full justify-center items-center ">
+                                <Card className="w-12 h-10 flex justify-center items-center rounded-r-none bg-stone-100">
+                                    <Link2Icon className="w-6 h-6" />
+                                </Card>
+                                <Card className="w-full h-10 flex justify-start items-center rounded-l-none focus: cursor-pointer hover:bg-stone-200">
+                                    <div className="ml-2">
+                                        {props.extraLink}
+                                    </div>
+                                </Card>
+                            </CardFooter>
+                        </Card>
+                    ) : null}
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default PostDetailDialog;
