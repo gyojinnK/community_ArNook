6d534af83f6ff64a6480420cad224ef63912eb58
gyojinnK <ksmug1998@gmail.com>
2024-02-08 00:31:32 +0900

    :memo: Add file
    댓글 리스트 뷰 컴포넌트 추가 및 optimistic update 구현 ( queryClient를 사용한 useMutation 구독)

---
+++ src/components/commentEls/CommentList.tsx
@@ -0,0 +1,67 @@
+import { db, getCommentDBRef } from "@/utils/firebase";
+import { collection, deleteDoc, getDocs } from "firebase/firestore";
+import { CommentData } from "@/vite-env";
+import { useQuery } from "react-query";
+import { useContext, useRef, useState } from "react";
+import CommentUpdate from "./CommentUpdate";
+import CommentDelete from "./CommentDelete";
+import { Separator } from "../ui/separator";
+import { AuthContext } from "@/store/AuthContext";
+import CommentElement from "./CommentElement";
+
+const CommentList: React.FC<{ email: string; postId: string }> = (props) => {
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    const fetchAllComment = async () => {
+        const querySnapshot = await getDocs(collection(db, "comment"));
+        let tempArr: CommentData[] = [];
+        querySnapshot.forEach((doc) => {
+            if (doc.data() && doc.data().postId === props.postId) {
+                const tempObj: CommentData = {
+                    writerEmail: doc.data().writerEmail,
+                    postId: doc.data().postId,
+                    commentId: doc.data().commentId,
+                    comment: doc.data().comment,
+                    likeCount: doc.data().likeCount,
+                    createdAt: doc.data().createdAt,
+                    updatedAt: doc.data().updatedAt,
+                };
+                tempArr.push(tempObj);
+            }
+        });
+        if (tempArr) {
+            tempArr.sort(
+                (a: CommentData, b: CommentData) =>
+                    b.createdAt.toDate().getTime() -
+                    a.createdAt.toDate().getTime()
+            );
+        }
+        scrollRef.current!.scrollIntoView({ behavior: "smooth" });
+        return tempArr;
+    };
+
+    const { data: comments } = useQuery<CommentData[]>(
+        "comments",
+        fetchAllComment
+    );
+
+    return (
+        <div className="max-h-44 overflow-scroll">
+            <div ref={scrollRef}></div>
+            {comments ? (
+                comments.map((commentInfo) => (
+                    <CommentElement
+                        key={commentInfo.commentId}
+                        commentInfo={commentInfo}
+                        email={props.email}
+                        postId={props.postId}
+                    />
+                ))
+            ) : (
+                <div>댓글이 없습니다.</div>
+            )}
+        </div>
+    );
+};
+
+export default CommentList;
