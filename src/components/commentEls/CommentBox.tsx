ab09721ab29488820a60e59fd01769e68a008471
gyojinnK <ksmug1998@gmail.com>
2024-02-08 00:26:53 +0900

    :memo: Add file
    댓글창 인터페이스 추가

---
+++ src/components/commentEls/CommentBox.tsx
@@ -0,0 +1,48 @@
+import { ChatBubbleIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+import { useEffect } from "react";
+import { db } from "@/utils/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { CommentData } from "@/vite-env";
+import { useQuery } from "react-query";
+
+const CommentBox: React.FC<{ onOpen: () => void; postId: string }> = (
+    props
+) => {
+    const fetchComnet = async () => {
+        const querySnapshot = await getDocs(collection(db, "comment"));
+        let tempArr: CommentData[] = [];
+        querySnapshot.forEach((doc) => {
+            if (doc.data() && doc.data().postId === props.postId) {
+                const tempObj: CommentData = {
+                    writerEmail: doc.data().writerEmail,
+                    postId: doc.data().postId,
+                    commentId: doc.data().commentId,
+                    comment: doc.data().comment,
+                    likeCount: doc.data().likeCount,
+                    createdAt: doc.data().createdAt,
+                    updatedAt: doc.data().updatedAt,
+                };
+                tempArr.push(tempObj);
+            }
+        });
+        return tempArr;
+    };
+
+    const { data: comments } = useQuery<CommentData[]>("comments", fetchComnet);
+
+    return (
+        <div className="text-right">
+            <Button
+                onClick={props.onOpen}
+                variant="outline"
+                className="rounded-xl ml-8 px-3 py-1 w-18 h-6"
+            >
+                <ChatBubbleIcon className="inline-block mr-2" />
+                {comments?.length}
+            </Button>
+        </div>
+    );
+};
+
+export default CommentBox;
