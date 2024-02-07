b740b4d015cf9cff6d161647409c19bc9f0cdf61
gyojinnK <ksmug1998@gmail.com>
2024-02-08 00:32:51 +0900

    :memo: Add file
    댓글창 카드 UI 컴포넌트 생성

---
+++ src/components/commentEls/CommentWrap.tsx
@@ -0,0 +1,27 @@
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
+import CommentForm from "./CommentForm";
+import CommentList from "./CommentList";
+
+const CommentWrap: React.FC<{ email: string; postId: string }> = (props) => {
+    return (
+        <Card>
+            <CardHeader className="py-3">
+                <CardTitle className="text-lg">댓글</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <CommentList email={props.email} postId={props.postId} />
+            </CardContent>
+            <CardFooter>
+                <CommentForm email={props.email} postId={props.postId} />
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default CommentWrap;
