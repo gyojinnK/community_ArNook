b39b2edd18a3a6f09f7f765a8cea7450993608c6
gyojinnK <ksmug1998@gmail.com>
2024-02-12 22:58:42 +0900

    :memo: Add file
    대댓글 UI 인테페이스 개발

---
+++ src/components/commentEls/SubCommentElement.tsx
@@ -0,0 +1,26 @@
+import { SubCommentData } from "@/vite-env";
+import SubCommentDelete from "./SubCommentDelete";
+
+const SubCommentElement: React.FC<{
+    subCom: SubCommentData;
+    postId: string;
+    commentId: string;
+}> = (props) => {
+    return (
+        <div className="my-2 flex justify-between items-center">
+            <div>
+                <div className=" text-xs text-stone-400">
+                    {props.subCom.writerEmail}
+                </div>
+                <div className=" text-sm">{props.subCom.subComment}</div>
+            </div>
+            <SubCommentDelete
+                subCom={props.subCom}
+                postId={props.postId}
+                commentId={props.commentId}
+            />
+        </div>
+    );
+};
+
+export default SubCommentElement;
