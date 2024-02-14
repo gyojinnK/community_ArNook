7bf031cca663c3a9fcc66d04e194b25188906962
gyojinnK <ksmug1998@gmail.com>
2024-02-14 15:57:18 +0900

    :memo: Update file
    QueryClientProvider 전역 변경으로 삭제

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,11 @@
 import Layout from "@/components/layout/Layout";
 import ListAllFeed from "@/components/mainEls/ListAllFeed";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 const MainPage = () => {
-    const queryClient = new QueryClient();
     return (
         <>
             <Layout>
-                <QueryClientProvider client={queryClient}>
-                    <ListAllFeed />
-                </QueryClientProvider>
+                <ListAllFeed />
             </Layout>
         </>
     );
