4b494e42421d88f9e68b8880defebb3d1459f37c
gyojinnK <ksmug1998@gmail.com>
2024-02-15 20:54:16 +0900

    :memo: Update file
    http 에러 처리

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-    GithubAuthProvider,
-    GoogleAuthProvider,
-    User,
-    getAuth,
-} from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth } from "firebase/auth";
 import {
     DocumentData,
     DocumentReference,
