ec789125bcc0432d5c6202812326770dd9cedfea
gyojinnK <ksmug1998@gmail.com>
2024-02-15 20:28:55 +0900

    :memo: Update file
    유저 프로필 이미지 로딩 전 로딩 인터페이스 추가

--- src/components/detailEls/UserAvatar.tsx
+++ src/components/detailEls/UserAvatar.tsx
@@ -1,14 +1,15 @@
-import { Avatar, AvatarImage } from "../ui/avatar";
-import demoImg from "@/assets/vector/defaultProfileImage.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Skeleton } from "../ui/skeleton";
 
 const UserAvatar: React.FC<{
-    curPImageId: string | null | undefined;
+    curPImageId: string;
 }> = (props) => {
     return (
         <Avatar className="w-64 h-64 my-6 mx-10 xl:w-72 xl:h-72">
-            <AvatarImage
-                src={props.curPImageId ? props.curPImageId : demoImg}
-            />
+            <AvatarImage src={props.curPImageId} />
+            <AvatarFallback>
+                <Skeleton className="h-full w-full rounded-full bg-stone-800" />
+            </AvatarFallback>
         </Avatar>
     );
 };
