1edf40c86bd17f686460eb4a6c867d8695e772ad
gyojinnK <ksmug1998@gmail.com>
2024-02-21 18:33:11 +0900

    :memo: Update file
    Mutation 적용하여 즉각적인 UI 변경 개선

--- src/components/postEls/LikeWrap.tsx
+++ src/components/postEls/LikeWrap.tsx
@@ -1,8 +1,9 @@
 import { HeartIcon } from "@radix-ui/react-icons";
 import { Badge } from "../ui/badge";
 import { getFeedDBRef } from "@/utils/firebase";
-import { getDoc, increment, updateDoc } from "firebase/firestore";
+import { increment, updateDoc } from "firebase/firestore";
 import { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
 
 const LikeWrap: React.FC<{
     email: string;
@@ -10,24 +11,41 @@ const LikeWrap: React.FC<{
     likeCount: number;
 }> = (props) => {
     const [likeCnt, setLikeCnt] = useState<number>(props.likeCount);
+    const queryClient = useQueryClient();
 
     const increasingLikeCountHandler = async () => {
         const feadRef = getFeedDBRef(props.email + "|" + props.postId);
         await updateDoc(feadRef, {
             likeCount: increment(1),
         });
-
-        const snapshot = await getDoc(feadRef);
-        if (snapshot) {
-            setLikeCnt(snapshot.data()?.likeCount);
-        }
+        setLikeCnt((prev) => prev + 1);
     };
+
+    const mutation = useMutation(increasingLikeCountHandler, {
+        onMutate: async () => {
+            await queryClient.cancelQueries("likeCnt");
+            const previousLikeCnt = queryClient.getQueryData("likeCnt");
+            queryClient.setQueriesData<number>("likeCnt", (old) =>
+                old ? old + 1 : 1
+            );
+            return { previousLikeCnt };
+        },
+        onError: (error, _, context) => {
+            if (context?.previousLikeCnt) {
+                console.error(error);
+                queryClient.setQueriesData("likeCnt", context.previousLikeCnt);
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries("likeCnt");
+        },
+    });
 
     return (
         <div className="select-none ">
             <Badge
                 variant="outline"
-                onClick={increasingLikeCountHandler}
+                onClick={() => mutation.mutate()}
                 className="focus: cursor-pointer hover:bg-red-100 rounded-xl px-3 py-1 w-18 h-6"
             >
                 <HeartIcon className="text-red-600 mr-2" />
