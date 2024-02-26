2d1a53fe79a840bc4773de084b66caa20ff6fc42
gyojinnK <ksmug1998@gmail.com>
2024-02-26 19:06:28 +0900

    :memo: Update file
    좋아요 수 mutation 적용 및 조건부 렌더링 추가

--- src/components/postEls/LikeWrap.tsx
+++ src/components/postEls/LikeWrap.tsx
@@ -1,56 +1,141 @@
-import { HeartIcon } from "@radix-ui/react-icons";
+import { HeartFilledIcon, HeartIcon } from "@radix-ui/react-icons";
 import { Badge } from "../ui/badge";
-import { getFeedDBRef } from "@/utils/firebase";
-import { useState } from "react";
-import { useMutation, useQueryClient } from "react-query";
+import { db, getFeedDBRef } from "@/utils/firebase";
+import { useContext, useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { AuthContext } from "@/store/AuthContext";
+import {
+    arrayRemove,
+    arrayUnion,
+    doc,
+    getDoc,
+    increment,
+    updateDoc,
+} from "firebase/firestore";
 
 const LikeWrap: React.FC<{
     email: string;
     postId: string;
     likeCount: number;
+    flagString: string;
 }> = (props) => {
-    const [likeCnt, setLikeCnt] = useState<number>(props.likeCount);
+    const curUser = useContext(AuthContext);
+    // const [likeCnt, setLikeCnt] = useState<number>(props.likeCount);
+    const [likedPosts, setLikedPosts] = useState<string[]>([]);
+    const [isliked, setIsLiked] = useState<boolean>(false);
     const queryClient = useQueryClient();
 
-    const increasingLikeCountHandler = async () => {
-        const { increment, updateDoc } = await import("firebase/firestore");
+    const fetchLikes = async () => {
+        // API 호출 등을 통해 좋아요 수를 가져옵니다.
         const feadRef = getFeedDBRef(props.email + "|" + props.postId);
-        await updateDoc(feadRef, {
-            likeCount: increment(1),
-        });
-        setLikeCnt((prev) => prev + 1);
+        const feedData = await getDoc(feadRef);
+        const curLikeCount: number = feedData.data()?.likeCount;
+        return curLikeCount;
     };
 
-    const mutation = useMutation(increasingLikeCountHandler, {
+    const { data: likes } = useQuery([props.postId, "likeCnt"], fetchLikes);
+
+    const likeCountHandler = async () => {
+        if (!isliked) {
+            if (curUser?.email) {
+                const userRef = doc(db, "users", curUser.email);
+                await updateDoc(userRef, {
+                    likedPost: arrayUnion(props.postId),
+                });
+            }
+
+            const feadRef = getFeedDBRef(props.email + "|" + props.postId);
+            await updateDoc(feadRef, {
+                likeCount: increment(1),
+            });
+            // setLikeCnt((prev) => prev + 1);
+            setIsLiked(true);
+        } else {
+            if (curUser?.email) {
+                const userRef = doc(db, "users", curUser.email);
+                await updateDoc(userRef, {
+                    likedPost: arrayRemove(props.postId),
+                });
+            }
+            const feadRef = getFeedDBRef(props.email + "|" + props.postId);
+            await updateDoc(feadRef, {
+                likeCount: increment(-1),
+            });
+            // setLikeCnt((prev) => prev - 1);
+            setIsLiked(false);
+        }
+    };
+
+    const mutation = useMutation(likeCountHandler, {
         onMutate: async () => {
-            await queryClient.cancelQueries("likeCnt");
-            const previousLikeCnt = queryClient.getQueryData("likeCnt");
-            queryClient.setQueriesData<number>("likeCnt", (old) =>
-                old ? old + 1 : 1
+            await queryClient.cancelQueries([props.postId, "likeCnt"]);
+            const previousLikeCnt = queryClient.getQueryData([
+                props.postId,
+                "likeCnt",
+            ]);
+            queryClient.setQueriesData<number>(
+                [props.postId, "likeCnt"],
+                (old) =>
+                    old && isliked ? old + 1 : old && !isliked ? old - 1 : 0
             );
             return { previousLikeCnt };
         },
         onError: (error, _, context) => {
             if (context?.previousLikeCnt) {
                 console.error(error);
-                queryClient.setQueriesData("likeCnt", context.previousLikeCnt);
+                queryClient.setQueriesData(
+                    [props.postId, "likeCnt"],
+                    context.previousLikeCnt
+                );
             }
         },
         onSuccess: () => {
-            queryClient.invalidateQueries("likeCnt");
+            queryClient.invalidateQueries([props.postId, "likeCnt"]);
         },
     });
 
+    const fetchLikeData = async () => {
+        if (curUser?.email) {
+            const docRef = doc(db, "users", curUser.email);
+            const likedPostDatas = await getDoc(docRef);
+            setLikedPosts(likedPostDatas.data()?.likedPost);
+        }
+    };
+
+    useEffect(() => {
+        fetchLikeData();
+    }, []);
+
+    useEffect(() => {
+        if (likedPosts && likedPosts.includes(props.postId)) {
+            setIsLiked(true);
+        }
+    }, [likedPosts]);
+
     return (
         <div className="select-none ">
-            <Badge
-                variant="outline"
-                onClick={() => mutation.mutate()}
-                className="focus: cursor-pointer hover:bg-red-100 rounded-xl px-3 py-1 w-18 h-6"
-            >
-                <HeartIcon className="text-red-600 mr-2" />
-                <div className="text-stone-600">{likeCnt}</div>
-            </Badge>
+            {props.flagString === "summary" ? (
+                <Badge
+                    variant="outline"
+                    onClick={() => mutation.mutate()}
+                    className="focus: cursor-pointer hover:bg-red-100 rounded-xl px-3 py-1 w-18 h-6"
+                >
+                    <HeartIcon className="text-red-600 mr-2" />
+                    <div className="text-stone-600">{likes}</div>
+                </Badge>
+            ) : props.flagString === "detail" ? (
+                <Badge
+                    variant="outline"
+                    onClick={() => mutation.mutate()}
+                    className="focus: cursor-pointer hover:bg-red-100 rounded-xl px-3 py-1 w-18 h-6"
+                >
+                    {isliked ? (
+                        <HeartFilledIcon className="text-red-600 mx-1 " />
+                    ) : (
+                        <HeartIcon className="text-red-600 mx-1 " />
+                    )}
+                </Badge>
+            ) : null}
         </div>
     );
 };
